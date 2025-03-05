be7487bb24c24d2556ae483164e0c9395ea17d7c
alstonwang <wanga23@wfu.edu>
2025-03-05 15:19:46 -0500

    Started assignment. Will finish later tonight or tomorrow.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,15 +1,15 @@
-import java . util .*;
+import java.util.*;
 
 class Node{
     int data;
-    Node next=null;
+    Node next = null;
 
     public Node(int i){
-        data=i;
+        data = i;
     }
     public Node(int i, Node n){
-        data=i;
-        next=n;
+        data = i;
+        next = n;
     }
 }
 public class LinkedList {
@@ -18,19 +18,116 @@ public class LinkedList {
         Random rand = new Random();
         Node head = new Node(rand.nextInt(10));
         Node curr = head;
+
         for (int i = 0; i < 9; i++) {
             Node temp = new Node(rand.nextInt(10));
             curr.next = temp;
             curr = curr.next;
         }
 
-        //print
         curr = head;
+
         while (curr != null) {
-            System.out.println(curr.data);
+            System.out.print(curr.data + " ");
             curr = curr.next;
         }
 
+        System.out.println();
+
+        System.out.print("Enter the x that you would like to delete: ");
+        Scanner scnr = new Scanner(System.in);
+        int x = 0;
+        boolean error;
+
+        do {
+            try {
+                x = scnr.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.print("Invalid. Try again: ");
+                error = true;
+            }
+        } while(error); // Retrieves valid input from the user.
+
+        curr = head;
+        Node prev = head;
+
+        while (curr != null) {
+            if (curr.data == x) {
+                if (prev == head) {
+                    head = head.next;
+                } else {
+                    prev.next = curr.next;
+                    curr.next = null;
+                }
+                break;
+            }
+            prev = curr;
+            curr = curr.next;
+        } // Deletes x of the user's choice.
+
+        curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        } // Prints out the content of the LinkedList.
+
+        System.out.println();
+
+        /*
+
+        head = head.next; // Deletes the head.
+
+        curr = head;
+        while (curr != null) {
+            prev = curr;
+            curr = curr.next;
+            if (curr == null) {
+                prev.next = null;
+            }
+        } // Deletes the last Node.
+
+        curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        } // Prints out the content of the LinkedList.
+
+        System.out.println();
+
+        Node temp = head;
+        head.data = 20;
+        head.next = temp;
+        // Inserts Node with item 20 at the head.
+
+        curr = head;
+        boolean stop = false;
+        while (!stop) {
+            curr = curr.next;
+            if (curr.next == null) {
+                curr.next = new Node(40, null);
+                stop = true;
+            }
+        } // Inserts Node with item 40 at the end.
+
+        curr = head;
+        int count = 0;
+        while (curr != null && count < 4) {
+            prev = curr;
+            if (count == 3) {
+                prev = new Node(60, curr.next);
+            } else {
+                curr = curr.next;
+            }
+            count++;
+        } // Inserts Node with item 60 as the fourth Node.
+
+        curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        } // Prints out the content of the LinkedList.
+         */
     }
 
 }
